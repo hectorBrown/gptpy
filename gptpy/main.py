5c26ea8d08fb7442f6bcae772038a21ce030e65e
Hector Brown <hectorjbrown@protonmail.com>
2023-02-17 00:58:06 +0000

    up max tokens so always stops

--- gptpy/main.py
+++ gptpy/main.py
@@ -8,6 +8,6 @@ def start(args=sys.argv[1:]):
     parser.add_argument("prompt")
     args = vars(parser.parse_args(args))
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    response = openai.Completion.create(model="text-davinci-003", prompt=args["prompt"], temperature=0).choices[0].text[2:]
-    print(response)
+    response = openai.Completion.create(model="text-davinci-003", prompt=args["prompt"], temperature=0, max_tokens=2048)
+    print(response.choices[0].text[2:])
 
